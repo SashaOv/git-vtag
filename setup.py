ad5375d71df0a55bd515dc3a255be91d4717f62d
Sasha Ovsankin <sasha@ovsankin.com>
2016-05-15 00:20:38 -0700

    Fix the build and version number. TODO: how can we do automatically?

--- setup.py
+++ setup.py
@@ -4,13 +4,15 @@ from os import path
 
 current_directory = path.abspath(path.dirname(__file__))
 
+readme_file= 'README.rst'
+
 def read_long_description():
-  with open(path.join(current_directory, 'README.md'), encoding='utf-8') as input:
+  with open(path.join(current_directory, readme_file), encoding='utf-8') as input:
     return input.read()
 
 setup(
   name='gitvtag',
-  version= '0.1',
+  version= '0.2',
 
   description='A git subcommand for simple and straightforward version tagging',
   long_description=read_long_description(),
@@ -20,7 +22,7 @@ setup(
   author_email= 'sasha@ovsankin.com',
   license= 'Apache License, Version 2.0',
   packages= find_packages(),
-  package_data={ '': ['README.rst', 'LICENSE'] },
+  package_data={ '': [readme_file, 'LICENSE'] },
   install_requires= [ ],
   entry_points={
     'console_scripts': [
