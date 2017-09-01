72f5e265fab044532f99d1e45dbd47d735d1128c
Sasha Ovsankin <sasha@ovsankin.com>
2017-09-01 15:09:05 -0700

    Fix dependency

---
+++ test/test_git.py
@@ -0,0 +1,23 @@
+import unittest
+
+from deploy2.Git import Git
+
+
+class TestTags(unittest.TestCase):
+  def setUp(self):
+    self.git= Git()
+
+  def test_increment(self):
+    self.assertEqual(self.git.increment_tag("v1"), "v2")
+    self.assertEqual(self.git.increment_tag("v0.1"), "v0.2")
+    self.assertEqual(self.git.increment_tag("v15.1-rc"), "v15.2-rc")
+    self.assertEqual(self.git.increment_tag("v2.3.4-beta5"), "v2.3.4-beta6")
+    with  self.assertRaises(ValueError) as dummy:
+      self.git.increment_tag("vOne-beta-five")
+
+  def test_last_tag(self):
+    pass  # TODO
+
+
+if __name__ == "__main__":
+  unittest.main()
